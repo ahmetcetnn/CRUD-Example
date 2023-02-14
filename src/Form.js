c4fd0e88136ebf95bbfce9491454c43c2412ccee
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-14 22:12:46 +0300

    form verilerini ayrı bir compa geçirdim

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,20 @@ import React from 'react'
 function Form() {
   return (
     <div>
-      
+       <form>
+        <input type="text" name="isim" value={form.isim} onChange={handleChange} placeholder='İsim Soyisim Giriniz'/>
+        <select name="sehir" value={form.sehir} onChange={handleChange}>
+          <option value="">Lütfen Şehir Seçiniz</option>
+          <option value="Kilis">Kilis</option>
+          <option value="Ankara">Ankara</option>
+          <option value="İstanbul">İstanbul</option>
+
+        </select>
+        <input type="date" name="dogum" value={form.dogum} onChange={handleChange}/>
+        <input type="color" name="renk" value={form.renk} onChange={handleChange}/>
+        <textarea name="adres" placeholder='Adresinizi Giriniz' value={form.adres} onChange={handleChange}/>
+        <Link to ="/bilgiler" ><button>Gönder</button></Link>
+      </form>
     </div>
   )
 }
