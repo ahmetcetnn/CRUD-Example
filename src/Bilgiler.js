9ad719494dba83886ef3ab7f4097dbd125f00d36
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-14 22:09:55 +0300

    React router dom ile veriyi componente geçirdim

---
+++ src/Bilgiler.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Bilgiler({form}) {
+  return (
+    <div>
+      <h2>İsim Soyisim:{form.isim} </h2>
+      <p>Şehir:</p>
+      <p></p>
+    </div>
+  )
+}
+
+export default Bilgiler
