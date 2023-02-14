b8eadbeadb1aac8a4f89a95450065531df722f05
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-14 21:36:29 +0300

    install rrd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
 function App() {
+const [form,setForm]=useState({isim:'', sehir:'', dogum:'', renk:'', adres:''});
+
+const handleChange=(event) =>{
+  setForm({...form,[event.target.name]:event.target.value})
+  console.log(form)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Bilgilerini Giriniz</h2>
+      <form>
+        <input type="text" name="isim" value={form.isim} onChange={handleChange} placeholder='İsim Soyisim Giriniz'/>
+        <select name="sehir" value={form.sehir} onChange={handleChange}>
+          <option value="">Lütfen Şehir Seçiniz</option>
+          <option value="Kilis">Kilis</option>
+          <option value="Ankara">Ankara</option>
+          <option value="İstanbul">İstanbul</option>
+
+        </select>
+        <input type="date" name="dogum" value={form.dogum} onChange={handleChange}/>
+        <input type="color" name="renk" value={form.renk} onChange={handleChange}/>
+        <textarea name="adres" placeholder='Adresinizi Giriniz' value={form.adres} onChange={handleChange}/>
+        <button>Gönder</button>
+      </form>
     </div>
   );
 }
